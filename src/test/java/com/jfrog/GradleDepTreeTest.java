c8d91e53a372a9a333d05bbba29a898659b38e2b
Bassel Mbariky <basselm@jfrog.com>
2025-10-05 11:40:52 +0300

    Fixed-tests

--- src/test/java/com/jfrog/GradleDepTreeTest.java
+++ src/test/java/com/jfrog/GradleDepTreeTest.java
@@ -2,48 +2,64 @@ package com.jfrog;
 
 import com.jfrog.tasks.GenerateDepTrees;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.testfixtures.ProjectBuilder;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class GradleDepTreeTest {
 
-    private Project project;
-    private GradleDepTree plugin;
-
-    @BeforeMethod
-    public void setUp() {
-        project = ProjectBuilder.builder().build();
-        plugin = new GradleDepTree();
+    @AfterMethod
+    public void tearDown() {
+        System.clearProperty(GenerateDepTrees.CURATION_AUDIT_MODE);
     }
 
     @Test
-    public void testApplyCreatesTask() {
-        // Apply the plugin
-        plugin.apply(project);
-        
-        // Verify that the task was created
-        assertNotNull(project.getTasks().findByName(GenerateDepTrees.TASK_NAME));
-    }
-
-    @Test
-    public void testCurationAuditModeConstant() {
-        // Verify that the constant is properly defined
-        assertEquals(GenerateDepTrees.CURATION_AUDIT_MODE, "com.jfrog.curationAuditMode");
-    }
-
-    @Test
-    public void testCurationModeDetection() {
-        // Test that curation mode can be detected
+    public void testCurationModeAddsApiPrefix() {
+        Project project = ProjectBuilder.builder().build();
+        project.getRepositories().maven(repo -> repo.setUrl("https://test.jfrog.io/artifactory/libs-release"));
         System.setProperty(GenerateDepTrees.CURATION_AUDIT_MODE, "true");
         
-        try {
-            boolean isCurationMode = Boolean.parseBoolean(System.getProperty(GenerateDepTrees.CURATION_AUDIT_MODE, "false"));
-            assertTrue(isCurationMode);
-        } finally {
-            System.clearProperty(GenerateDepTrees.CURATION_AUDIT_MODE);
-        }
+        new GradleDepTree().apply(project);
+
+        MavenArtifactRepository repo = (MavenArtifactRepository) project.getRepositories().iterator().next();
+        assertEquals(repo.getUrl().toString(), "https://test.jfrog.io/artifactory/api/curation/audit/libs-release");
+    }
+
+    @Test
+    public void testNoCurationModeNoChange() {
+        Project project = ProjectBuilder.builder().build();
+        project.getRepositories().maven(repo -> repo.setUrl("https://test.jfrog.io/artifactory/libs-release"));
+        
+        new GradleDepTree().apply(project);
+
+        MavenArtifactRepository repo = (MavenArtifactRepository) project.getRepositories().iterator().next();
+        assertEquals(repo.getUrl().toString(), "https://test.jfrog.io/artifactory/libs-release");
+    }
+
+    @Test
+    public void testNonArtifactoryUrlUnchanged() {
+        Project project = ProjectBuilder.builder().build();
+        project.getRepositories().maven(repo -> repo.setUrl("https://repo1.maven.org/maven2/"));
+        System.setProperty(GenerateDepTrees.CURATION_AUDIT_MODE, "true");
+        
+        new GradleDepTree().apply(project);
+
+        MavenArtifactRepository repo = (MavenArtifactRepository) project.getRepositories().iterator().next();
+        assertEquals(repo.getUrl().toString(), "https://repo1.maven.org/maven2/");
+    }
+
+    @Test
+    public void testAlreadyModifiedUrlNotDuplicated() {
+        Project project = ProjectBuilder.builder().build();
+        project.getRepositories().maven(repo -> repo.setUrl("https://test.jfrog.io/artifactory/api/curation/audit/libs-release"));
+        System.setProperty(GenerateDepTrees.CURATION_AUDIT_MODE, "true");
+        
+        new GradleDepTree().apply(project);
+
+        MavenArtifactRepository repo = (MavenArtifactRepository) project.getRepositories().iterator().next();
+        assertEquals(repo.getUrl().toString(), "https://test.jfrog.io/artifactory/api/curation/audit/libs-release");
     }
 }
